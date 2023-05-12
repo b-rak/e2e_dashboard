2909f7bf88ae8102d9c2153e27e21a475261306f
Burak Tas <burak.tas@appmatics.de>
2023-05-12 11:35:57 +0200

    add page testfälle

---
+++ components/OS_Kachel.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="os-1" class="w-[355px] h-[151px] bg-white rounded-2xl shadow-[2px_-2px_8px_rgba(0,0,0,0.08),-2px_2px_4px_rgba(0,0,0,0.08)] my-auto flex flex-col gap-3">
+        <div class="flex flex-row justify-between items-center mx-4 my-2">
+            <span class="font-lato not-italic font-bold text-lg leading-8">
+                <font-awesome-icon :icon="{prefix: 'fas', iconName: 'desktop'}" class="text-[#222F34] h-[18px] w-[22px]"/>
+                {{os_name}}
+            </span>
+            <div class="bg-[#DFE4E7] rounded-lg text-center h-7 w-[101px] p-[2px] font-lato not-italic font-bold text-sm leading-6">
+                {{numberOfCases > 1 ? numberOfCases + ' Cases' : '1 Case'}}
+            </div>
+        </div>
+        <div class="h-3/5 flex flex-row justify-between items-center mx-4">
+            <div class="h-full w-[165px] bg-slate-200 text-center flex flex-row justify-center items-center">Chart JS</div>
+
+            <div class="h-14 w-[104px] flex flex-col justify-center items-center">
+                <div class="flex flex-row justify-center items-center gap-2">
+                    <font-awesome-icon :icon="{prefix: 'fas', iconName: 'arrow-up'}" class="text-[#1BB398] h-[15px] w-[11px] "/>
+                    <span class="font-lato not-italic font-bold text-4xl leading-[32px] text-[#222F34]">30%</span>
+                </div>
+                <span class="font-lato not-italic font-normal text-sm leading-6 text-[#222F34]">Success Rate</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- params
+line 2: id-number 
+line 5: iconName and classes?
+line 6: OS Name 
+line 9: #cases -> line 2-9: depends on OS config; set only once and update if config gets updated
+line 13: chart -> depends on success rate
+line 17: iconName and classes? -> depends on success rate
+line 18: pass percentage  -> api request
+-->
+
+<script lang="ts" setup>
+defineProps({
+    id: Number,
+    iconName: String,
+    os_name: String,
+    numberOfCases: Number
+})
+
+
+</script>
---
+++ components/OS_Testcases.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-[#DFE4E7] mt-36 rounded-xl shadow-[0px_0px_4px_rgba(0,0,0,0.08)] pb-9 mx-[120px]">
+        <div class="flex flex-row justify-between items-center px-6 pt-6 pb-3">
+            <span class="font-lato not-italic font-medium text-[28px] leading-8">
+                <font-awesome-icon :icon="{prefix: 'fas', iconName: 'desktop'}" class="text-[#222F34] h-[38px] w-[39px]"/>
+                Web
+            </span>
+            <div class="bg-[#FCFCFC] rounded-xl text-center h-[37px] w-[136px] py-3 px-6 font-lato not-italic font-bold text-[18px] leading-[13px] text-[#222F34]">
+                12 Cases
+            </div>
+        </div>
+        <div class="py-0 px-6 mt-6 flex flex-row flex-wrap gap-x-[26px] gap-y-6">
+            <Testcase-Kachel v-for="index in 10" :key="index" :id="index"/>
+        </div>
+    </div>
+</template>
