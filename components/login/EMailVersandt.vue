bcf1c8858cda0387cc858f606c6c461bc48d7758
burak.tas <burak.tas@appmatics.de>
2023-07-18 09:50:14 +0200

    add loading animation

--- components/login/EMailVersandt.vue
+++ components/login/EMailVersandt.vue
@@ -8,14 +8,31 @@
     </div>
     <span class="display_semibold_48 my-6">E-Mail versandt!</span>
     <span class="h2_medium_28 basic_text_grey mb-[4.5rem]"
-      >Wir haben eine E-Mail versandt an:</span
+      >Wenn Ihre E-Mail-Adresse in unserem System registriert ist, haben wir
+      eine E-Mail versandt an:</span
     >
     <span class="h2_bold_28 basic_text_grey mb-6">{{ email }}</span>
-    <div class="text_regular_16 basic_text_grey p-2">
+    <div
+      class="text_regular_16 basic_text_grey py-2 pr-2 pl-[9rem] flex items-center gap-2"
+    >
       Keine E-Mail erhalten?
-      <span class="cursor-pointer status_text_fail_100 pl-2"
-        >Erneut Senden?</span
-      >
+      <span
+        v-if="!displaySendAgain"
+        class="cursor-pointer status_text_fail_100 pl-2"
+        @click="loadingAnimation"
+        >Erneut senden
+        <font-awesome-icon
+          v-if="displayAnimation"
+          :icon="['far', 'spinner-third']"
+          class="text-[1rem] animate-spin"
+      /></span>
+      <span v-else class="basic_text_grey pl-2 inline-flex items-center gap-2"
+        >Erneut versandt
+        <font-awesome-icon
+          :icon="['far', 'circle-check']"
+          class="status_text_pass_100 text-[1rem]"
+        />
+      </span>
     </div>
     <span
       class="text_regular_16 cursor-pointer f_text_neutral_500 mt-[4.5rem]"
@@ -29,9 +46,22 @@
 const props = defineProps<{
   email: String;
 }>();
+const displaySendAgain = ref(false);
+const displayAnimation = ref(false);
 
 const emits = defineEmits(["goTo:login"]);
 const goToLogin = () => {
   emits("goTo:login");
+};
+
+const loadingAnimation = () => {
+  displayAnimation.value = true;
+  setTimeout(() => {
+    displayAnimation.value = false;
+    displaySendAgain.value = true;
+    setTimeout(() => {
+      displaySendAgain.value = false;
+    }, 2000);
+  }, 2000);
 };
 </script>
