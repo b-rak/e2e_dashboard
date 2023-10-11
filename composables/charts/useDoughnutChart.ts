f2ff5d15b82c5ce536363e401602e7647103aec3
burak.tas <burak.tas@appmatics.de>
2023-10-11 16:30:47 +0200

    give data values minimum size

--- composables/charts/useDoughnutChart.ts
+++ composables/charts/useDoughnutChart.ts
@@ -5,21 +5,45 @@ import {
   statusPass100,
 } from "./chartConfig";
 
+const checkFraction = (elementIndex: number, data: [number, number]) => {
+  if (data[elementIndex] === 0) return 0;
+  else
+    return data[elementIndex] / (data[0] + data[1]) < 0.1
+      ? Math.round((data[0] + data[1]) / 20)
+      : data[elementIndex];
+};
+
 // Details Verteilung Chart
 export default async (data: [number, number], update: Boolean) => {
-  // Doughnut Chart
+  // create an array with objects of the original data value and minimum sized value
+  // the minimum sized value is needed to give each data value a minimum fraction for displaying reasons (currently each data value will be displayed at least as 5% of the half-doughnut)
+  // the original value is needed so we can still show the correct value on the tooltip
+  const dataWithMinimumSize = [
+    {
+      original: data[0],
+      current: checkFraction(0, data),
+    },
+    {
+      original: data[1],
+      current: checkFraction(1, data),
+    },
+  ];
   if (update) {
+    // update chart data and label for tooltip
     let chart = Chart.getChart("doughnut") as Chart;
-    chart.data.datasets[0].data = data;
+    chart.data.datasets[0].data = dataWithMinimumSize.map((x) => x.current);
+    chart.config.options.plugins.tooltip.callbacks.label = (context) =>
+      context.label + ": " + dataWithMinimumSize[context.dataIndex].original;
     chart.update();
   } else {
+    // Doughnut Chart
     new Chart(document.getElementById("doughnut") as HTMLCanvasElement, {
       type: "doughnut",
       data: {
         labels: ["Passed", "Failed"],
         datasets: [
           {
-            data: data,
+            data: dataWithMinimumSize.map((x) => x.current),
             backgroundColor: [statusPass100, statusFail100],
             borderJoinStyle: "round",
           },
@@ -47,7 +71,11 @@ export default async (data: [number, number], update: Boolean) => {
                 return "";
               },
               label: (context) => {
-                return context.label + ": " + context.parsed;
+                return (
+                  context.label +
+                  ": " +
+                  dataWithMinimumSize[context.dataIndex].original
+                );
               },
             },
           },
