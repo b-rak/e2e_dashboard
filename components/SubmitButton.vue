4736b10db53ff61b1bb502ffacf8e609a2c3a911
burak.tas <burak.tas@appmatics.de>
2023-11-16 11:58:24 +0100

    px to rem

--- components/SubmitButton.vue
+++ components/SubmitButton.vue
@@ -2,7 +2,7 @@
 <template>
   <button
     type="submit"
-    class="mt-12 border_medium f_secondary_00 w-full py-[15px] button_semibold_16 basic_text_white"
+    class="mt-12 border_medium f_secondary_00 w-full py-4 button_semibold_16 basic_text_white"
   >
     {{ buttonText }}
     <slot></slot>
