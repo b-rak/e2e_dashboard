57e0d95564a0541302dc086f97bd25543da8d897
burak.tas <burak.tas@appmatics.de>
2024-01-23 16:37:23 +0100

    update Footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -3,15 +3,33 @@
     class="h-[4.625rem] flex items-center py-[1.312rem] text_regular_18"
     :class="[
       breakpoint.viewport === 'xs' || breakpoint.viewport === 'sm'
-        ? 'flex-col-reverse'
+        ? 'flex-col-reverse justify-center'
         : 'justify-between',
     ]"
   >
-    <span class="f_text_neutral_700 h-8">2023 ©appmatics</span>
+    <span class="f_text_neutral_700 h-8">© 2024 appmatics</span>
     <div class="flex justify-between w-[22.375rem] f_text_neutral_900 h-8">
-      <span class="cursor-pointer">Support</span>
-      <span class="cursor-pointer">Report a problem</span>
-      <span class="cursor-pointer">Contact us</span>
+      <NuxtLink
+        to="https://www.appmatics.de"
+        id="appmatics-homepage"
+        class="cursor-pointer f_text_neutral_700"
+        target="_blank"
+        >Appmatics</NuxtLink
+      >
+      <NuxtLink
+        to="https://www.appmatics.com/de/datenschutz"
+        id="appmatics-homepage"
+        class="cursor-pointer f_text_neutral_700"
+        target="_blank"
+        >Datenschutz</NuxtLink
+      >
+      <NuxtLink
+        to="https://www.appmatics.com/de/impressum"
+        id="appmatics-homepage"
+        class="cursor-pointer f_text_neutral_700"
+        target="_blank"
+        >Impressum</NuxtLink
+      >
     </div>
   </footer>
 </template>
