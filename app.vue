d0457119e438d9ce8a03aa373b6362d697d21ee8
burak.tas <burak.tas@appmatics.de>
2024-02-06 16:13:01 +0100

    check logged in state and get config on app start

--- app.vue
+++ app.vue
@@ -3,3 +3,40 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+console.log("APP START");
+const configStore = useConfigStore();
+const dashboardCasesStore = useDashboardCasesStore();
+
+const user = await useFetch("/api/user", { method: "GET" });
+
+if (user.data.value) {
+  const token = user.data.value.projectToken;
+  let configName = "";
+  if (token === "ALL") {
+    configName = "rheinbahn.config.json";
+  } else if (token === "AVV") {
+    configName = "avv.config.json";
+  } else if (token === "BVG") {
+    configName = "bvg.config.json";
+  } else if (token === "DEMO") {
+    configName = "appmaticsdemo.config.json";
+  } else if (token === "KVB") {
+    configName = "kvb.config.json";
+  } else if (token === "MIGROS") {
+    configName = "migros.config.json";
+  } else if (token === "RHEINBAHN") {
+    configName = "rheinbahn.config.json";
+  } else if (token === "RMV") {
+    configName = "rmv.config.json";
+  } else if (token === "SCHAFERSHOP") {
+    configName = "schaefershop.config.json";
+  }
+
+  await configStore.loadConfigFile(configName);
+  await dashboardCasesStore.getDashboardCases(configName);
+} else {
+  navigateTo("/login");
+}
+</script>
